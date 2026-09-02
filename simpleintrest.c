416447b85f269b3897f631f4af52a1513b3a626e
jitendrakumawat96967 <jitendrakumawat96967@gmail.com>
2026-09-02 20:09:43 +0530

    Add simple interest calculation program in C

---
+++ simpleintrest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+float p,r,t,si;
+
+printf("enter value of p");
+scanf("%f",&p);
+printf("enter value of r");
+scanf("%f",&r);
+printf("enter value of t");
+scanf("%f",&t);
+ si=(p*r*t)/100;
+printf("si:%f",si);
+
+    return 0;
+}
