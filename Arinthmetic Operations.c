2a4f6796ac2d9a16823603c8ae0564bbe1a43a9d
jitendrakumawat96967 <jitendrakumawat96967@gmail.com>
2026-09-03 05:26:03 +0530

    Create Arinthmetic Operations.c

---
+++ Arinthmetic Operations.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+int x,y;
+printf("Arithmetic Operations\n");
+printf("value of x :");
+scanf("%d",&x);
+printf("value of y :");
+scanf("%d",&y);
+printf("x+y :%d\n",x+y);
+printf("x-y :%d\n",x-y);
+printf("x*y :%d\n",x*y);
+printf("x/y :%d\n",x/y);
+printf("x%y :%d\n",x%y);
+    return 0;
+}
