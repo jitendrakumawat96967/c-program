070b45fb00d0504d6d71dfddb461b45241ab97b6
jitendrakumawat96967 <jitendrakumawat96967@gmail.com>
2026-09-02 20:32:54 +0530

    Create areaofcricle.c

---
+++ areaofcricle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+float pi,radios,area;
+printf("enter radios");
+scanf("%f",&radios);
+pi=3.14;
+area=pi*radios*radios;
+printf("area=%f",area);
+    return 0;
+}
