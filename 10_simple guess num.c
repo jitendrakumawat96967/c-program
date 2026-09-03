4e5b095e6390f9a5d2e1392373e20d1d52174b02
jitendrakumawat96967 <jitendrakumawat96967@gmail.com>
2026-09-03 17:55:47 +0530

    Create 10_simple guess num.c

---
+++ 10_simple guess num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf(" gusses number between 0-10: ");
+    scanf("%d",&num);
+    if(num==6)
+    {
+        printf("you gusses is right ");
+    }
+    else
+    {
+        printf("your gusses is worng");
+    }
+}
