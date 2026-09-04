248970b9d399951ca2d23e6f670793e3b1fe7cab
jitendrakumawat96967 <jitendrakumawat96967@gmail.com>
2026-09-04 20:24:52 +0530

    Update 11_advance guess number.c

--- 11_advance guess number.c
+++ 11_advance guess number.c
@@ -1,17 +1,25 @@
+
 #include <stdio.h>
 int main() {
-    int n;
-    printf("Guess the number between(1-10) :\n ");
-    while(1){
-        scanf("%d",&n);
-     if(n==9)
-     {
-        printf("\nyou gussed it right");
-        break;
-     }
-     else{
-        printf("\nworng guess, try again : ");
-     }
+    int num;
+    int secret = 9;   
+
+    printf("Guess the number between 1-10 (0 to exit):\n");
+
+    while(1) {
+        scanf("%d", &num);
+
+        if(num == 0) {
+            printf("Game exited!\n");
+            break;
+        } else if(num == secret) {
+            printf("You guessed it right!\n");
+            break;
+        } else if(num < secret) {
+            printf("Too low, try again:\n");
+        } else {
+            printf("Too high, try again:\n");
+        }
     }
 
     return 0;
